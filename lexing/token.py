2f059e9450ee158243d97262221280bd58924f0f
David <davidjmacdonald11@gmail.com>
2023-01-22 13:08:22 -0500

    Add lexer

---
+++ lexing/token.py
@@ -0,0 +1,19 @@
+class Token:
+    def __init__(self, kind, line):
+        self.kind = kind
+        self.line = line
+        self.locale, self.string = line.new_locale()
+
+        self.string = self.string or "EOF"
+
+    def __repr__(self):
+        return f"{self.kind[0]}'{self.string}'"
+
+    def has(self, *strings):
+        return self.string in strings
+
+    def of(self, *kinds):
+        return self.kind in kinds
+
+    def mark(self):
+        self.line.mark(self)
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from util.error import LanguageError
+from lexing.lexer import Lexer
+from lexing.source_line import SourceLine
+
 def main():
-    print("Hello World!")
+    print("Arithmetic 1.0:")
+    lexer = Lexer()
+
+    while True:
+        line = input("> ")
+        if line == "exit": break
+        if line == "": continue
+
+        try:
+            line = SourceLine(line)
+            tokens = lexer.make_tokens(line)
+            print(tokens)
+        except LanguageError as error:
+            print(error)
 
 if __name__ == "__main__":
     main()
