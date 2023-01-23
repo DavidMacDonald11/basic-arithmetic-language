e512296992f93d016772c518fe3a63a0d5711718
David <davidjmacdonald11@gmail.com>
2023-01-23 13:53:54 -0500

    End of episode 2

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from lexing.lexer import Lexer
+from lexing.source_line import SourceLine
+from util.error import LanguageError
+
 def main():
-    print("Hello World!")
+    print("Arithmetic 1.0: ")
+    lexer = Lexer()
+
+    while True:
+        line = input("> ")
+        if line == "exit": break
+
+        try:
+            line = SourceLine(line)
+            tokens = lexer.make_tokens(line)
+            print(tokens)
+        except LanguageError as error:
+            print(error)
 
 if __name__ == "__main__":
     main()
